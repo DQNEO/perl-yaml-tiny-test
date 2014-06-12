e8a8084e714b93f4cf00060c0c03f79557094703
DQNEO <dqneoo@gmail.com>
2014-06-12 12:29:48 +0900

    fix to work on cygwin

--- least.pl
+++ least.pl
@@ -5,6 +5,11 @@ use warnings;
 use YAML::Tiny;
 use Encode;
 
-my $yamltext = Encode::encode('UTF8',do {local $/; open my $fh,'<:encoding(cp932)', "sjis.yml"; <$fh>;});
-my $tree = YAML::Tiny->read_string($yamltext);
-print $tree->[0]{'str'};
+# on cygwin
+my $output_enc = 'utf8';
+# on active perl
+# my $output_enc = 'cp932';
+
+my $string =do {local $/; open my $fh,'<:encoding(cp932)', "sjis.yml"; <$fh>;};
+my $tree = YAML::Tiny->read_string($string);
+print Encode::encode($output_enc, $tree->[0]{'key'}), "\n";
